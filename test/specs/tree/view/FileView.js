29a2476adb9a42de00bfb31e20a0c46df989f842
Stephen Bradshaw <stephen.bradshaw@hotmail.co.uk>
2012-11-01 21:30:25 +0000

    Pass mock event to fix failing tests

--- test/specs/tree/view/FileView.js
+++ test/specs/tree/view/FileView.js
@@ -1,10 +1,11 @@
 require([
 	"underscore",
+	"jquery",
 	"tree/view/FileView",
 	"tree/view/FolderView",
 	"tree/model/Tree",
 	"tree/model/File"
-], function (_, FileView, FolderView, Tree, File) {
+], function (_, $, FileView, FolderView, Tree, File) {
 	describe("tree/view/FileView", function () {
 
 		it("instantiates with a model", function () {
@@ -17,7 +18,7 @@ require([
 			var model = new File(),
 				view = new FileView({ model: model });
 
-			view.render().edit();
+			view.render().edit($.Event());
 
 			chai.assert.isTrue(view.$el.find(".title").hasClass("hidden"));
 			chai.assert.isFalse(view.$el.find(".title-edit").hasClass("hidden"));
@@ -27,8 +28,8 @@ require([
 			var model = new File(),
 				view = new FileView({ model: model });
 
-			view.render().edit();
-			view.cancelEdit();
+			view.render().edit($.Event());
+			view.cancelEdit($.Event());
 
 			chai.assert.isFalse(view.$el.find(".title").hasClass("hidden"));
 			chai.assert.isTrue(view.$el.find(".title-edit").hasClass("hidden"));
